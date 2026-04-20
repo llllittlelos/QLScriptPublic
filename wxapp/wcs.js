f0253fe94bc6523a2a1918c1b231f1325b787092
smallfawn <860562056@QQ.COM>
2026-04-20 15:19:54 +0800

    Update wcs.js

--- wxapp/wcs.js
+++ wxapp/wcs.js
@@ -6,14 +6,15 @@ class WeChatCodeServer {
         this.auth = options.auth;
     }
     getCode(openid) {
+        console.log('等待获取code:');
         return new Promise((resolve, reject) => {
             axios.post(this.serverUrl + '/wx/code', { appid: this.appid, openid }, {
                 headers: {
                     'auth': this.auth
                 },
-                timeout: 30 * 1000
+                timeout: 10 * 1000
             }).then(res => {
-
+                console.log('获取code成功:');
                 resolve(res);
             }).catch(err => {
                 reject(err);
@@ -21,13 +22,15 @@ class WeChatCodeServer {
         });
     }
     init(openid) {
+        console.log('等待初始化:');
         return new Promise((resolve, reject) => {
             axios.post(this.serverUrl + '/init', { appid: this.appid, openid }, {
                 headers: {
                     'auth': this.auth
-                }
+                },
+                timeout: 10 * 1000
             }).then(res => {
-
+                console.log('初始化成功:');
                 resolve(res);
             }).catch(err => {
                 reject(err);
@@ -35,13 +38,15 @@ class WeChatCodeServer {
         });
     }
     close(openid) {
+        console.log('等待关闭:');
         return new Promise((resolve, reject) => {
-            axios.post(this.serverUrl + '/close', { appid: this.appid,openid: openid }, {
+            axios.post(this.serverUrl + '/close', { appid: this.appid, openid: openid }, {
                 headers: {
                     'auth': this.auth
-                }
+                },
+                timeout: 10 * 1000
             }).then(res => {
-
+                console.log('关闭成功:');
                 resolve(res);
             }).catch(err => {
                 reject(err);
@@ -49,13 +54,15 @@ class WeChatCodeServer {
         });
     }
     cloudInit(openid) {
+        console.log('等待云函数初始化:');
         return new Promise((resolve, reject) => {
             axios.post(this.serverUrl + '/wx/call/init', { appid: this.appid, openid }, {
                 headers: {
                     'auth': this.auth
                 },
-                timeout: 30 * 1000
+                timeout: 10 * 1000
             }).then(res => {
+                console.log('云函数初始化成功:');
                 resolve(res);
             }).catch(err => {
                 reject(err);
@@ -63,13 +70,15 @@ class WeChatCodeServer {
         });
     }
     cloudCall(openid) {
+        console.log('等待云函数调用:');
         return new Promise((resolve, reject) => {
             axios.post(this.serverUrl + '/wx/cloud/call', { appid: this.appid, openid }, {
                 headers: {
                     'auth': this.auth
                 },
-                timeout: 30 * 1000
+                timeout: 10 * 1000
             }).then(res => {
+                console.log('云函数调用成功:');
                 resolve(res);
             }).catch(err => {
                 reject(err);
